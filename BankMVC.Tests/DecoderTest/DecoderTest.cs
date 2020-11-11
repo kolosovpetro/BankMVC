072e9185687a3e93f7d9d5500fe10666a9fc4e06
razumovskiy <kolosovp94@gmail.com>
2020-11-11 14:03:26 +0100

    simple decoder-encoder written

---
+++ BankMVC.Tests/DecoderTest/DecoderTest.cs
@@ -0,0 +1,17 @@
+﻿using BankMVC.Auxiliary.Decode;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BankMVC.Tests.DecoderTest
+{
+    [TestFixture]
+    public class DecoderTest
+    {
+        [Test]
+        public void Decoder_Simple_Test()
+        {
+            var decoder = new Decoder();
+            decoder.Decode("BCDE").Should().Be(1234);
+        }
+    }
+}
--- BankMVC.Tests/EncoderTest/EncoderTest.cs
+++ BankMVC.Tests/EncoderTest/EncoderTest.cs
@@ -1,10 +1,17 @@
-﻿using NUnit.Framework;
+﻿using BankMVC.Auxiliary.Encode;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace BankMVC.Tests.EncoderTest
 {
     [TestFixture]
     public class EncoderTest
     {
-        
+        [Test]
+        public void Encoder_Simple_Test()
+        {
+            var encoder = new Encoder();
+            encoder.Encode(1234).Should().Be("BCDE");
+        }
     }
 }
