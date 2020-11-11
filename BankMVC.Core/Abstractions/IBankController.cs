d18c7d7b564a83a010586d615813944ddd6f8783
razumovskiy <kolosovp94@gmail.com>
2020-11-11 15:00:06 +0100

    abstraction is done

---
+++ BankMVC.Core/Abstractions/IBankController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankMVC.Abstractions
+{
+    public interface IBankController
+    {
+        // login get
+        IActionResult Login();
+        // login post
+        IActionResult Login(IFormCollection collection);
+        // balance get
+        IActionResult Balance();
+        // balance post
+        IActionResult Balance(IFormCollection collection);
+    }
+}
