a19bbe2f200b62f69315ae48bc6833dde964d6e0
razumovskiy <kolosovp94@gmail.com>
2020-11-11 13:16:54 +0100

    initial commit

---
+++ BankMVC.Model/Models/Transaction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BankMVC.Model.Models
+{
+    public class Transaction
+    {
+        public string UserName { get; set; }    // foreign key
+        public double Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
---
+++ BankMVC.Model/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace BankMVC.Model.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Pin { get; set; }        // pin as a string since it will be decrypted
+        public double Balance { get; set; }
+    }
+}
