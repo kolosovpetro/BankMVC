a8b26f80b903a1204dce72f20e562efee9095ad5
razumovskiy <kolosovp94@gmail.com>
2020-11-11 22:04:34 +0100

    project finished

--- BankMVC.Core/Startup.cs
+++ BankMVC.Core/Startup.cs
@@ -17,7 +17,7 @@ namespace BankMVC
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
